5d9d99daefbfae897e880c7f9248749c7a7feaed
boraq999 <alharethalalem@gmail.com>
2024-03-06 14:50:59 +0200

    edit v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,11 +26,37 @@ const changeSlide = (direction) => {
     }
   }
 
-  slideRight.style.transform = `translateY(-${
-    activeSlideIndex * sliderHeight
-  }px)`;
-  slideLeft.style.transform = `translateY(${
-    activeSlideIndex * sliderHeight
-  }px)`;
+  slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight
+    }px)`;
+  slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight
+    }px)`;
 };
 
+
+
+// console.log(mobile_icon);
+
+let mobile_item = document.querySelectorAll('.mobile_item');
+
+
+document.addEventListener('click', function (event) {
+  console.log(event.target.classList.contains('mobile_item'));
+
+
+  if (event.target.classList.contains('mobile_item')) {
+    console.log(1);
+    mobile_active_remove();
+    event.target.classList.add('my_active');
+  }
+
+
+
+});
+
+
+
+function mobile_active_remove() {
+  document.querySelectorAll('.mobile_item').forEach(el => {
+    el.classList.remove('my_active');
+  });
+}
